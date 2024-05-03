c70bf6cf9e38bea1ab5348b2e9c436bcd1363a5c
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-05-03 20:08:08 +0700

    Initial Commit

---
+++ src/test/java/id/ac/ui/cs/rpl/flextime/FlextimeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.rpl.flextime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlextimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
