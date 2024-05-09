c0cabfc4830e33c056bb1a0fadc60370f1910610
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-05-09 20:23:15 +0700

    FEAT: Added models for the fitness plan module

---
+++ src/main/java/id/ac/ui/cs/rpl/flextime/model/Training.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.rpl.flextime.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "training")
+@Getter @Setter
+public class Training {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "customization_id",
+            referencedColumnName = "id"
+    )
+    private Customization customization;
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String trainingType;
+}
--- src/main/java/id/ac/ui/cs/rpl/flextime/model/init.java
+++
@@ -1,4 +0,0 @@
-package id.ac.ui.cs.rpl.flextime.model;
-
-public class init {
-}
