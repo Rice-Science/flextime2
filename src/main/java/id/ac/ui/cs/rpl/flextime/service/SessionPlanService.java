322f0584ed483320dd49995ffc166ee917e2f373
fiardiel <rafi.ardiel@ui.ac.id>
2024-05-17 02:09:53 +0700

    FEAT: Added additional front end elements

--- src/main/java/id/ac/ui/cs/rpl/flextime/service/SessionPlanService.java
+++ src/main/java/id/ac/ui/cs/rpl/flextime/service/SessionPlanService.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.rpl.flextime.service;
 import id.ac.ui.cs.rpl.flextime.model.SessionPlan;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface SessionPlanService {
@@ -10,4 +11,5 @@ public interface SessionPlanService {
     public Optional<SessionPlan> getSessionPlanById(String id);
     public List<SessionPlan> getAllSessionPlansByFitnessPlan(String id);
     public void deleteSessionPlanById(String id);
+    public int getTrainingCount(String id);
 }
